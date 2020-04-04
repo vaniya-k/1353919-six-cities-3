1c4e14cfbb9b629b9c481c7b08b8a16f8d84b24b
vaniya <vaniya.karakasiyan@gmail.com>
2020-04-04 14:47:50 +0300

    Тревис ругается на снапшоты..

--- src/components/reviews-list/reviews-list.test.js
+++ src/components/reviews-list/reviews-list.test.js
@@ -5,14 +5,14 @@ import ReviewsList from './reviews-list.jsx';
 const reviews = [
   {
     avaPicUrl: `https://localhost`,
-    date: new Date(`December 17, 2019 03:24:00`),
+    date: new Date(`December 17, 2019 00:24:00`),
     name: `Bruce`,
     rating: 1,
     text: `I couldn't stand beeing there -- it's like desert. Why would anyone live in such a place? It's beyond my comprehension`
   },
   {
     avaPicUrl: `https://localhost`,
-    date: new Date(`December 25, 2019 13:27:00`),
+    date: new Date(`December 25, 2019 10:27:00`),
     name: `Clark`,
     rating: 5,
     text: `Gorgeous ambience, so much light and air! You could even play soccer here!`
