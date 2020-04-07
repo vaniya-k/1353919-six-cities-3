eb4f886e8d46d3a673e121ef8d2745e8a0556563
vaniya <vaniya.karakasiyan@gmail.com>
2020-04-07 16:01:19 +0300

    Вынес стейт сортировки в НОС

--- src/components/places-list-sorting/places-list-sorting.jsx
+++ src/components/places-list-sorting/places-list-sorting.jsx
@@ -8,9 +8,6 @@ const SORT_TYPES = [`Popular`, `Price: low to high`, `Price: high to low`, `Top 
 class PlacesListSorting extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {
-      isOpened: false
-    };
   }
 
   generateStyleClasses = (activeSortType, itemSortType) => {
@@ -21,18 +18,12 @@ class PlacesListSorting extends React.PureComponent {
     }
   }
 
-  toggleDropDown = () => {
-    this.setState((prevState) => ({
-      isOpened: !prevState.isOpened
-    }));
-  }
-
   generateLiItems = () => {
     const liItems = [];
 
     for (let i = 0; i < SORT_TYPES.length; i++) {
       liItems.push(<li className={this.generateStyleClasses(this.props.activeSortType, i)} key={`key${i}`} onClick={() => {
-        this.props.onTypeItemClick(i); this.toggleDropDown();
+        this.props.onTypeItemClick(i); this.props.onDropdownToggle();
       }} tabIndex="0">{SORT_TYPES[i]}</li>);
     }
 
@@ -42,13 +33,13 @@ class PlacesListSorting extends React.PureComponent {
   render() {
     return <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by&nbsp;</span>
-      <span className="places__sorting-type" tabIndex="0" onClick={this.toggleDropDown}>
+      <span className="places__sorting-type" tabIndex="0" onClick={() => this.props.onDropdownToggle()}>
         {SORT_TYPES[this.props.activeSortType]}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use href="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className={`places__options places__options--custom ${(this.state.isOpened) ? `places__options--opened` : null}`}>
+      <ul className={`places__options places__options--custom ${(this.props.isOpened) ? `places__options--opened` : null}`}>
         {this.generateLiItems()}
       </ul>
     </form>;
---
+++ src/hocs/withDropdownStatusSwitcher/with-dropdown-status-switcher.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PlacesListSorting from '../../components/places-list-sorting/places-list-sorting.jsx';
+
+const withDropdownStatusSwitcher = (Component) => {
+  class DropdownStatusSwitcher extends React.PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        isOpened: false
+      }
+
+      this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    handleToggle () {
+      this.setState((prevState) => ({
+        isOpened: !prevState.isOpened
+      }));
+    }
+
+    render() {
+      return <Component {...this.props} isOpened={this.state.isOpened} onDropdownToggle={this.handleToggle}/>
+    }
+  }
+
+  return DropdownStatusSwitcher;
+}
+
+export default withDropdownStatusSwitcher(PlacesListSorting);
