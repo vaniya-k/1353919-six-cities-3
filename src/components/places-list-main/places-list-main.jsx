085896ad169a78050270d11be132c462c3640c7f
vaniya <vaniya.karakasiyan@gmail.com>
2020-04-08 22:52:11 +0300

    Наифинальнейший

--- src/components/places-list-main/places-list-main.jsx
+++ src/components/places-list-main/places-list-main.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import {ActionCreator as OffersActionCreator} from "../../reducer/offers/offers.js";
 import PlaceCard from '../place-card/place-card.jsx';
 import PlacesListSortingWrapped from '../../hocs/withDropdownStatusSwitcher/with-dropdown-status-switcher.jsx';
 
-class PlacesListMain extends React.PureComponent {
+class PlacesListMain extends React.Component {
   constructor(props) {
     super(props);
     this.myRef = React.createRef();
   }
 
-  componentDidUpdate() {
-    const {activeCityName} = this.props;
+  shouldComponentUpdate(nextProps) {
+    if (this.props.activeCityName !== this.cityNameBuffer || this.props.activeSortType !== nextProps.activeSortType || this.props.activeCityId !== nextProps.activeCityId || this.props.favsQuantity !== nextProps.favsQuantity) {
+      if (this.props.activeCityId !== nextProps.activeCityId) {
+        this.myRef.current.scrollIntoView();
+      }
 
-    this.shouldTriggerScrollUpOnCityChange(activeCityName);
-  }
-
-  cityNameBuffer = this.props.activeCityName;
-
-  shouldTriggerScrollUpOnCityChange = (cityName) => {
-    if (cityName !== this.cityNameBuffer) {
-      this.cityNameBuffer = cityName;
-
-      this.myRef.current.scrollIntoView();
+      return true;
+    } else {
+      return false;
     }
   }
 
+  handleHover = (placeLatLon) => {
+    const value = (placeLatLon === this.props.activeCardLatLon) ? {lat: null, lon: null} : placeLatLon;
+    this.props.setActiveCardLatLon(value);
+  }
+
   render() {
-    const {foundPlacesQuantity, activeCityName, places, onHover} = this.props;
+    const {activeCityName, places} = this.props;
 
     return <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
-      <b className="places__found" ref={this.myRef}>{foundPlacesQuantity} places to stay in {activeCityName}</b>
+      <b className="places__found" ref={this.myRef}>{places.length} places to stay in {activeCityName}</b>
       <PlacesListSortingWrapped/>
       <div className="cities__places-list places__list tabs__content">
-        {places.map((place, i) => <PlaceCard key={`key${i}`} placeLatLon={{lat: place.gps.lat, lon: place.gps.lon}} place={place} onHover={onHover}/>)}
+        {places.map((place, i) => <PlaceCard key={`key${i}`} placeLatLon={{lat: place.gps.lat, lon: place.gps.lon}} place={place} onHover={this.handleHover}/>)}
       </div>
     </section>;
   }
@@ -41,6 +44,8 @@ class PlacesListMain extends React.PureComponent {
 
 PlacesListMain.propTypes = {
   activeCityName: PropTypes.string.isRequired,
+  activeCityId: PropTypes.number.isRequired,
+  activeSortType: PropTypes.number.isRequired,
   places: PropTypes.arrayOf(
       PropTypes.shape({
         title: PropTypes.string.isRequired,
@@ -57,8 +62,62 @@ PlacesListMain.propTypes = {
         }).isRequired
       }).isRequired
   ).isRequired,
-  foundPlacesQuantity: PropTypes.number.isRequired,
-  onHover: PropTypes.func.isRequired
+  activeCardLatLon: PropTypes.shape({
+    lat: PropTypes.number,
+    lon: PropTypes.number
+  }),
+  favsQuantity: PropTypes.number.isRequired,
+  setActiveCardLatLon: PropTypes.func.isRequired
 };
 
-export default PlacesListMain;
+const mapStateToProps = (state) => {
+  const {places, allOffers, activeCityId, activeSortType} = state.offers;
+
+  const activeCityName = allOffers[activeCityId].city;
+
+  let sortedPlaces = [];
+
+  if (places) {
+    sortedPlaces = places;
+  }
+
+  const buildPlacesListing = (sortType) => {
+    switch (sortType) {
+      case 0:
+        break;
+      case 1:
+        sortedPlaces = places.sort((a, b) => (a.price > b.price) ? 1 : -1);
+        break;
+      case 2:
+        sortedPlaces = places.sort((a, b) => (a.price > b.price) ? -1 : 1);
+        break;
+      case 3:
+        sortedPlaces = places.sort((a, b) => (a.rating > b.rating) ? -1 : 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  buildPlacesListing(activeSortType);
+
+  const favsQuantity = sortedPlaces.filter((place) => place.isFavorite === true).length;
+
+  return {
+    allOffers: state.offers.allOffers,
+    activeCityId: state.offers.activeCityId,
+    activeCityName,
+    activeSortType,
+    places: sortedPlaces,
+    activeCardLatLon: state.offers.activeCardLatLon,
+    favsQuantity
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  setActiveCardLatLon(activeCardLatLon) {
+    dispatch(OffersActionCreator.setActiveCardLatLon(activeCardLatLon));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlacesListMain);
--- src/components/places-list-main/places-list-main.test.js
+++ src/components/places-list-main/places-list-main.test.js
@@ -6,51 +6,80 @@ import {Provider} from "react-redux";
 import {Router} from 'react-router-dom';
 import history from '../../history.js';
 
-const mockPlaces = [
-  {
-    id: 1,
-    title: `Solitude Fortress`,
-    price: 123,
-    type: `house`,
-    rating: 100,
-    isPremium: true,
-    isFavorite: true,
-    previewUrl: `https://`,
-    gps: {lat: 123, lon: 123}
-  },
-  {
-    id: 2,
-    title: `Daily Planet Office`,
-    price: 321,
-    type: `apartment`,
-    rating: 20,
-    isPremium: false,
-    isFavorite: true,
-    previewUrl: `https://`,
-    gps: {lat: 123, lon: 123}
-  }
-];
-
 const mockReducer = () => {
   const state = {
     offers: {
-      activeSortType: 0
+      activeCityName: `Metropolis`,
+      activeCityId: 0,
+      activeCardLatLon: {lat: null, lon: null},
+      activeSortType: 0,
+      places: [
+        {
+          id: 1,
+          title: `Solitude Fortress`,
+          price: 123,
+          type: `house`,
+          rating: 100,
+          isPremium: true,
+          isFavorite: true,
+          previewUrl: `https://`,
+          gps: {lat: 123, lon: 123}
+        },
+        {
+          id: 2,
+          title: `Daily Planet Office`,
+          price: 321,
+          type: `apartment`,
+          rating: 20,
+          isPremium: false,
+          isFavorite: true,
+          previewUrl: `https://`,
+          gps: {lat: 123, lon: 123}
+        }
+      ],
+      allOffers: [{
+        city: `Metropolis`,
+        cityLatLon: {lat: 123, lon: 123},
+        places: []
+      },
+      {
+        city: `Gotham`,
+        cityLatLon: {lat: 123, lon: 123},
+        places: []
+      },
+      {
+        city: `Utopia`,
+        cityLatLon: {lat: 123, lon: 123},
+        places: []
+      },
+      {
+        city: `Dystopia`,
+        cityLatLon: {lat: 123, lon: 123},
+        places: []
+      },
+      {
+        city: `Inferno`,
+        cityLatLon: {lat: 123, lon: 123},
+        places: []
+      },
+      {
+        city: `Limbo`,
+        cityLatLon: {lat: 123, lon: 123},
+        places: []
+      }]
     },
     user: {
       authorizationStatus: `AUTH`
     }
   };
-
   return state;
 };
 
 const mockStore = createStore(mockReducer);
 
-const mockFunc = jest.fn();
-
 it(`<PlacesListMain/> shows a couple of places in Metropolis`, () => {
   const tree = renderer
-    .create(<Provider store={mockStore}><Router history={history}><PlacesListMain places={mockPlaces} activeCityName={`Metropolis`} foundPlacesQuantity={mockPlaces.length} onHover={mockFunc}/></Router></Provider>)
+    .create(<Provider store={mockStore}><Router history={history}><PlacesListMain/></Router></Provider>)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
